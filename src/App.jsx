4114728421d6742dcdfaa910fb435d1f770e2d7e
KMG <KMG@KMGKOL-LT034>
2025-09-15 17:08:15 +0530

    push 18

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects</h2>
+      <h1>this is feature one Prakash</h1>
+    <h1>this is feature two</h1>
+    <h2>thios is feature 3</h2>
+    <h3>this is feature 4</h3> 
+    <h3>this is feature 5</h3> 
+    <h3>this is feature 6</h3> 
     </>
   )
 }
